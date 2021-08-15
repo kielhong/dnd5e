17ac12cb4638560a1b48404b7e348ea1b9e1e4a6
홍민형 <mh.hong@woowahan.com>
2021-08-16 00:52:35 +0900

    refactor : weapon damageRoll refactoring

--- src/main/kotlin/com/widehouse/dnd/Weapon.kt
+++ src/main/kotlin/com/widehouse/dnd/Weapon.kt
@@ -2,12 +2,8 @@ package com.widehouse.dnd
 
 import com.widehouse.dnd.dice.Dice
 
-class Weapon(val name: String, val damage: List<Int>, val itemType: String) {
-    fun damageRoll(): Int {
-        var sum = 0
-        for (dice in damage) {
-            sum += Dice().roll(dice)
-        }
-        return sum
-    }
+class Weapon(val name: String, private val damage: List<Int>, val itemType: String) {
+    fun damageRoll() = damage.stream()
+        .mapToInt { Dice().roll(it) }
+        .sum()
 }
