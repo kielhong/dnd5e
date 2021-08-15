388daf4f75b9a3e4dce95f5434423e37383c57a5
홍민형 <mh.hong@woowahan.com>
2021-08-15 11:43:55 +0900

    feat: ArmorClass

--- src/main/kotlin/com/widehouse/dnd/Ability.kt
+++ src/main/kotlin/com/widehouse/dnd/Ability.kt
@@ -1,4 +1,39 @@
 package com.widehouse.dnd
 
-class Ability(val str: Int) {
+open class Ability(private val score: Int) {
+    companion object {
+        fun builder(str: Int, dex: Int, con: Int, int: Int, wis: Int, cha: Int): Map<String, Ability> {
+            return mapOf(
+                "str" to Strength(str),
+                "dex" to Dexterity(dex),
+                "con" to Constitution(con),
+                "int" to Intelligence(int),
+                "wis" to Wisdom(wis),
+                "cha" to Charisma(cha)
+            )
+        }
+    }
+
+    fun score() = score
+
+    fun modifier() = (score - 10) / 2
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+        if (score != (other as Ability).score) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return score
+    }
 }
+
+class Strength(score: Int) : Ability(score)
+class Dexterity(score: Int) : Ability(score)
+class Constitution(score: Int) : Ability(score)
+class Intelligence(score: Int) : Ability(score)
+class Wisdom(score: Int) : Ability(score)
+class Charisma(score: Int) : Ability(score)
--- src/test/kotlin/com/widehouse/dnd/AttackTest.kt
+++ src/test/kotlin/com/widehouse/dnd/AttackTest.kt
@@ -8,26 +8,26 @@ import io.mockk.mockk
 class AttackTest: FunSpec ({
     test("Character attack target then target get damage") {
         val char = mockk<Character>()
-        val target = Character(Ability(14), 2)
+        val target = Character(ability = mapOf("str" to Strength(14)), 2)
         every { char.attack(target) }.returns(AttackResult(5))
         val result = char.attack(target)
         result.resolve() shouldBe 5
     }
 
-    test("Attack Roll") {
-        var char = Character(Ability(15), 2)
-        char.modifier() + char.proficiency shouldBe 4
-        char = Character(Ability(19), 5)
-        char.modifier() + char.proficiency shouldBe 9
+    test("Attack Roll then hit") {
+        val dice = mockk<Dice>()
+        val char = Character(ability = mapOf("str" to Strength(18)), 3, dice = dice)
+        every { dice.roll(20) }.returns(15)
+        val target = mockk<Character>()
+        every { target.armorClass() }.returns(16)
+
+        char.attackRoll(target) shouldBe true
     }
 
     test("Damage Roll") {
         val sword = mockk<Weapon>()
         every { sword.damageRoll() } returns 5
-        val char = Character(Ability(15), 2, weapon = sword)
+        val char = Character(ability = mapOf("str" to Strength(15)), 2, weapon = sword)
         char.damage() shouldBe 5
     }
 })
-// TODO : Attack Roll
-// TODO : hit target
-// TODO : roll dice
