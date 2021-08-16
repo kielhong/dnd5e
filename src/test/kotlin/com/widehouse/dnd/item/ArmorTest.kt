e98c04e96762454d0083d10a8f1e29f64200cd65
홍민형 <mh.hong@woowahan.com>
2021-08-16 23:49:07 +0900

    refactor: Weapon, Armor with Item

--- src/test/kotlin/com/widehouse/dnd/item/ArmorTest.kt
+++ src/test/kotlin/com/widehouse/dnd/item/ArmorTest.kt
@@ -2,25 +2,28 @@ package com.widehouse.dnd.item
 
 import com.widehouse.dnd.character.Character
 import com.widehouse.dnd.character.Dexterity
+import com.widehouse.dnd.item.ArmorType.HeavyArmor
+import com.widehouse.dnd.item.ArmorType.LightArmor
+import com.widehouse.dnd.item.ArmorType.MediumArmor
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 
 class ArmorTest : FunSpec({
     test("Light Armor, add dex modifier to base armor class") {
-        val armor = Armor(name = "Padded", category = "Light Armor", ac = 11)
+        val armor = Armor(name = "Padded", armorType = LightArmor, armorClass = 11)
         val char = Character(ability = mapOf("dex" to Dexterity(14)), 2, maxHitPoints = 20, armor = armor)
-        char.armorClass() shouldBe armor.ac + char.ability["dex"]!!.modifier()
+        char.armorClass() shouldBe armor.armorClass + char.ability["dex"]!!.modifier()
     }
 
     test("Medium Armor, add dex modifier to base armor class") {
-        val armor = Armor(name = "Hide", category = "Medium Armor", ac = 12)
+        val armor = Armor(name = "Hide", armorType = MediumArmor, armorClass = 12)
         val char = Character(ability = mapOf("dex" to Dexterity(18)), 2, maxHitPoints = 20, armor = armor)
-        char.armorClass() shouldBe armor.ac + 2
+        char.armorClass() shouldBe armor.armorClass + 2
     }
 
     test("Heavy Armor, does not add dex modifier to base armor class") {
-        val armor = Armor(name = "Chain Main", category = "Heavy Armor", ac = 16)
+        val armor = Armor(name = "Chain Main", armorType = HeavyArmor, armorClass = 16)
         val char = Character(ability = mapOf("dex" to Dexterity(18)), 2, maxHitPoints = 20, armor = armor)
-        char.armorClass() shouldBe armor.ac
+        char.armorClass() shouldBe armor.armorClass
     }
 })
