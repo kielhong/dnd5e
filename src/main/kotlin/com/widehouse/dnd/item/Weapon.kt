6dda540b2d8a732766a3fc8fbd503cdff314f33d
kielhong <kielhong@naver.com>
2021-08-17 23:41:47 +0900

    feat : Weapon property

--- src/main/kotlin/com/widehouse/dnd/item/Weapon.kt
+++ src/main/kotlin/com/widehouse/dnd/item/Weapon.kt
@@ -3,8 +3,18 @@ package com.widehouse.dnd.item
 import com.widehouse.dnd.dice.Dice
 import com.widehouse.dnd.dice.Die
 
-class Weapon(name: String, private val damage: List<Die>, val itemType: String) : Item(name) {
+class Weapon(name: String, private val damage: List<Die>, val itemType: String, val properties: List<WeaponProperty> = listOf()) : Item(name) {
     fun damageRoll() = damage.stream()
         .mapToInt { Dice().roll(it) }
         .sum()
+}
+
+enum class WeaponProperty {
+    Finesse,
+    Light,
+    Heavy,
+    Range,
+    Thrown,
+    TwoHanded,
+    Versatile,
 }
