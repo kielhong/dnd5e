2ec597357ca713418dd2fede33f24c3c24a45e4e
kielhong <kielhong@naver.com>
2021-08-22 11:06:57 +0900

    refactor : Dice 실행 테스트를 reflection 으로 처리

--- src/test/kotlin/com/widehouse/dnd/character/MonsterTest.kt
+++ src/test/kotlin/com/widehouse/dnd/character/MonsterTest.kt
@@ -2,6 +2,8 @@ package com.widehouse.dnd.character
 
 import com.widehouse.dnd.character.MonsterFixtures.Companion.goblin
 import com.widehouse.dnd.character.Size.Small
+import com.widehouse.dnd.dice.Dice
+import com.widehouse.dnd.dice.Die.D20
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 import io.mockk.every
@@ -21,11 +23,16 @@ class MonsterTest : FunSpec({
     }
 
     test("when monster attack, attack roll + hit bonus") {
+        val dice = mockk<Dice>()
+        every { dice.roll(D20) }.returns(15)
         val character = mockk<Character>()
         every { character.armorClass }.returns(15)
         val action = mockk<Action>()
         every { action.hitBonus }.returns(5)
         val monster = spyk(Monster("goblin", Small, "Humanoid", Abilities(8, 14, 10, 10, 8, 8), 7, 15, action), recordPrivateCalls = true)
+        val f = Monster::class.java.getDeclaredField("dice")
+        f.isAccessible = true
+        f.set(monster, dice)
         monster.attackRoll(character)
 
         verify { action.hitBonus }
--- src/test/kotlin/com/widehouse/dnd/combat/AttackTest.kt
+++ src/test/kotlin/com/widehouse/dnd/combat/AttackTest.kt
@@ -6,13 +6,15 @@ import com.widehouse.dnd.character.CharacterFixtures.Companion.cleric
 import com.widehouse.dnd.character.CharacterFixtures.Companion.fighter
 import com.widehouse.dnd.character.CharacterFixtures.Companion.rogue
 import com.widehouse.dnd.character.CharacterFixtures.Companion.wizard
-import com.widehouse.dnd.dice.Dice
-import com.widehouse.dnd.dice.Die.D20
+import com.widehouse.dnd.character.Monster
+import com.widehouse.dnd.item.ItemFixtures
 import com.widehouse.dnd.item.Weapon
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 import io.mockk.every
 import io.mockk.mockk
+import io.mockk.spyk
+import io.mockk.verify
 
 class AttackTest : FunSpec({
     test("Character attack target then target get damage") {
@@ -26,13 +28,17 @@ class AttackTest : FunSpec({
     }
 
     test("Attack Roll then hit") {
-        val dice = mockk<Dice>()
-        every { dice.roll(D20) }.returns(15)
-        val target = mockk<Character>()
+        val target = mockk<Monster>()
         every { target.armorClass }.returns(16)
-        val char = fighter(dice = dice)
-
-        char.attackRoll(target) shouldBe true
+        val char = spyk(fighter(), recordPrivateCalls = true)
+        val weapon = ItemFixtures.longSword
+        char.equip(weapon)
+        // when
+        char.attackRoll(target)
+        // then
+        verify { char["attackModifier"](weapon) }
+        verify { char.proficiency() }
+        verify { target.armorClass }
     }
 
     test("Damage Roll") {
